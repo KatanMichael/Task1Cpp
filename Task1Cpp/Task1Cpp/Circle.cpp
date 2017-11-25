2265b0f992f8bd8ca851d4b11be3013d58227a84
KatanMichael <32782995+KatanMichael@users.noreply.github.com>
2017-11-25 21:40:19 +0200

    דרך נוספת לחישוב המרחק בלי math.h
    אני לא יודע אם זה בסדר להוסיף את math.h
    אז הנה עוד דרך... מקווה שזה עובד כי כתבתי את זה דרך האתר.

--- Task1Cpp/Task1Cpp/Circle.cpp
+++ Task1Cpp/Task1Cpp/Circle.cpp
@@ -24,12 +24,27 @@ int Circle::getColor() const { return color; }
 
 void Circle::setColor(int color) { this->color = color; }
 
-bool Circle::contains(const Point & p) const {
+bool Circle::contains(const Point & p) const 
+{
+	int x, y, r;
+	r = 
 	int x1 = center.getX(), y1 = center.getY();
 	int x2 = p.getX(),		y2 = p.getY();
-	double distance = sqrt(pow((x1 - x2), 2.0) + pow((y1 - y2), 2.0));
+	x = x1 - x2;
+	y = y1 - y2;
+	x = x*x;
+	y = y*y;
+	r = radius*radius;
+	
+	if(r <= (x+y))
+	{
+		return true;	
+	}
+	return false;
+	
+//	double distance = sqrt(pow((x1 - x2), 2.0) + pow((y1 - y2), 2.0));
 	// distance = sqrt((x1-x2)^2 + (y1-y2)^2)
-	return (double)radius >= distance ? true : false;
+//	return (double)radius >= distance ? true : false;
 }
 
 void Circle::print() const {
